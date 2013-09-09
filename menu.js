2bd0cf6b885e7c2d8e99a254cd3be54b35218cc9
Kiril <vatev.1@gmail.com>
2013-09-08 22:14:57 -0400

    reworked code

--- menu.js
+++ menu.js
@@ -1,28 +1,32 @@
 (function(){
+	//helpers
 	function divver(){ return document.createElement('div'); }
 	
-	var Constructor = function(options){
-		if (options.items){
+	function kill(ev){
+		if(ev) {
+			if (ev.stopPropagation) ev.stopPropagation();
+			if (ev.preventDefault) ev.preventDefault();
+			if (ev.preventCapture) ev.preventCapture();
+			if (ev.preventBubble) ev.preventBubble();
 			
+			// just in case
+			return false;
 		}
 	}
+	//end helpers
 	
-	function Menu(){
-		var menu = divver();
-		menu.className = "menu";
-		
-		//add functions
-		menu.items = [{
-			display: "First item",
-			event: function(ev){
-				c.log(ev);
-			}
-		},{
-			display: "Second item",
-			event: function(ev){
-				c.log(ev);
-			}
-		}];
+	//global menu -- allow only one in page
+	var menu = divver();
+	menu.className = "menu";
+	//hidden by default
+	menu.classList.add("hidden");
+	
+	//place menu in document
+	document.body.appendChild(menu);
+	
+	var Constructor = function(options){
+		//reset menu
+		menu.innerHTML = "";
 		
 		function appendItem(item){
 			var div = divver();
@@ -40,76 +44,62 @@
 			menu.appendChild(div);
 		}
 		
-		for (i in menu.items){ appendItem(menu.items[i]); }
-					
-		menu.show = function show(){
-			this.classList.remove('hidden');
-			
-			var position = function(){
-				var e = window.event;
-				//click location
-				var y = e.y || e.pageY || e.offsetY;
-				var x = e.x || e.pageX || e.offsetX;
-				c.log({x:x, y:y});
-				
-				//window frame
-				var frameX = window.innerWidth;
-				var frameY = window.innerHeight;
-				
-				//menu dimensions
-				var menuY = menu.offsetHeight || menu.clientHeight;
-				var menuX = menu.offsetWidth || menu.clientWidth;
-				
-				//check that menu does not go off screen
-				if ((x+menuX) > frameX) x = frameX - menuX;
-				if ((y+menuY) > frameY) y = y - menuY;
-				
-				//set menu position
-				menu.style.left = x + 'px';
-				menu.style.top = y + 'px';
-			}
-			position();
+		if (options.items){
+			for (i in options.items){ appendItem(options.items[i]); }
 		}
-		menu.hide = function hide(ev){
-			this.classList.add('hidden');
-		}
-		return menu;
+		
+		
+		//activate menu and event listeners
+		document.addEventListener("mousedown", function mouseDown(ev){
+			//righClick
+			if (ev.which === 3) showMenu(ev);
+			else if (ev.target.className === "item") c.log('menu item clicked');
+			else hideMenu(ev);
+		});
+		//document.onmousedown = mouseDown;
+		document.oncontextmenu = kill;
+		
 	}
-
-	var menu = new Menu();
-
-	function kill(ev){
-		if(ev) {
-			if (ev.stopPropagation) ev.stopPropagation();
-			if (ev.preventDefault) ev.preventDefault();
-			if (ev.preventCapture) ev.preventCapture();
-			if (ev.preventBubble) ev.preventBubble();
+	
+	menu.show = function show(){
+		this.classList.remove('hidden');
+		
+		var position = function(){
+			var e = window.event;
+			//click location
+			var y = e.y || e.pageY || e.offsetY;
+			var x = e.x || e.pageX || e.offsetX;
+			c.log({x:x, y:y});
 			
-			// just in case
-			return false;
+			//window frame
+			var frameX = window.innerWidth;
+			var frameY = window.innerHeight;
+			
+			//menu dimensions
+			var menuY = menu.offsetHeight || menu.clientHeight;
+			var menuX = menu.offsetWidth || menu.clientWidth;
+			
+			//check that menu does not go off screen
+			if ((x+menuX) > frameX) x = frameX - menuX;
+			if ((y+menuY) > frameY) y = y - menuY;
+			
+			//set menu position
+			menu.style.left = x + 'px';
+			menu.style.top = y + 'px';
 		}
+		position();
+	}
+	menu.hide = function hide(ev){
+		this.classList.add('hidden');
 	}
 
 	function showMenu(ev){
 		//kill event
 		kill(ev);
-		
 		menu.show();
-		
-		document.body.appendChild(menu);
 	}
 
-	function hideMenu(){
-		menu.hide();
-	}
+	function hideMenu(){ menu.hide(); }
 
-	document.addEventListener("mousedown", mouseDown);
-	//document.onmousedown = mouseDown;
-	document.oncontextmenu = kill;
-	function mouseDown(ev) {
-		//righClick
-		if (ev.which === 3) showMenu(ev);
-		else if (ev.target.className === "item") c.log('menu item clicked');
-		else hideMenu(ev);
-	}
+	window.menu = Constructor;
 })();
